5770e93bf02fc0089cdcbd18cd4a095c43e79a36
Gianna Fusaro <giannafusaro@Giannas-MacBook-Pro.local>
2015-11-05 06:54:08 -0800

    add validations, flash errors, and additional styling to movies index

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,13 @@
 class Review < ActiveRecord::Base
+
+  validates :email, presence: true, format: {
+    with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+    message: 'is invalid'
+  }
+
+  validates :rating, inclusion: {
+    in: (1..10).to_a,
+    message: 'must be between 1 and 10'
+  }
+
 end
