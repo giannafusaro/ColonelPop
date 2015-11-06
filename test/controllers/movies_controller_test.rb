c4a7b84bf02808d61fc6512f479ae67846967f0d
Gianna Fusaro <giannafusaro@Giannas-MacBook-Pro.local>
2015-11-06 12:28:10 -0800

    Add basic controller tests and a couple review fixtures

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class MoviesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "get index" do
+    get :index
+    assert_response :success
+    assert_select 'tbody tr', count: 15
+  end
+
+  test "get show" do
+    get :show, id: 9627
+    assert_response :success
+    assert_select 'article', count: 2
+  end
 end
--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class ReviewsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "new review" do
+    get :new, movie_id: 9627
+    assert_response :success
+
+    assert assigns(:movie)
+    assert assigns(:review)
+    assert_select '#review_movie_id[value="9627"]', 1
+  end
+
+  test "create valid review" do
+    post :create, movie_id: 9627, review: {
+      email: "example@example.com",
+      rating: "8",
+      comment: "It was okay.",
+      movie_id: "9627"
+    }
+    assert_response :redirect
+    assert_redirected_to root_path
+  end
+
+  test "create invalid review" do
+    post :create, movie_id: 9627, review: {
+      email: "invalid",
+      rating: "123",
+      comment: "",
+      movie_id: "9627"
+    }
+    assert_response :success
+    assert_select '.alert ul li', 2
+  end
+
 end
