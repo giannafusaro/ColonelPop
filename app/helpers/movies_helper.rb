c48036edc62d9b596c1298fdc8514804a77dd712
Gianna Fusaro <giannafusaro@Giannas-MacBook-Pro.local>
2015-11-06 17:07:34 -0800

    Add number_of_movies and rating_in_stars helpers

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,7 @@
 module MoviesHelper
+
+  def number_of_reviews(movie_id)
+    Review.where(movie_id: movie_id).count
+  end
+
 end
--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,15 @@
 module ReviewsHelper
+
+
+  def rating_in_stars(review)
+    content_tag(:h6) do
+      review.rating.times.collect do
+        "<i class=\"fa fa-star\"></i>"
+      end.join.html_safe +
+      (10-review.rating).times.collect do
+        "<i class=\"fa fa-star-o\"></i>"
+      end.join.html_safe
+    end
+  end
+
 end
